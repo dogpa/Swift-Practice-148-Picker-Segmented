a93a66593302e4deef241bddd8b59e75e85fde37
Dogpa <dogpa0130@gmail.com>
2022-04-19 07:09:02 +0800

    Initial Commit

---
+++ Swift Practice 148 Picker Segmented/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift Practice 148 Picker Segmented
+//
+//  Created by Dogpa's MBAir M1 on 2022/4/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift Practice 148 Picker Segmented/Swift_Practice_148_Picker_SegmentedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Practice_148_Picker_SegmentedApp.swift
+//  Swift Practice 148 Picker Segmented
+//
+//  Created by Dogpa's MBAir M1 on 2022/4/19.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Practice_148_Picker_SegmentedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
