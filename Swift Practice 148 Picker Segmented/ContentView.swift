de87e171d56d6508c329ad0a47cdf052296713f4
Dogpa <dogpa0130@gmail.com>
2022-04-20 21:52:13 +0800

    demo

--- Swift Practice 148 Picker Segmented/ContentView.swift
+++ Swift Practice 148 Picker Segmented/ContentView.swift
@@ -1,4 +1,4 @@
-//
+
 //  ContentView.swift
 //  Swift Practice 148 Picker Segmented
 //
@@ -8,9 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    //初始對SegmentedController做外觀設定
+    init(){
+        UISegmentedControl.appearance().selectedSegmentTintColor = .systemCyan
+        UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor.black], for: .selected)
+    }
+    
+    @State private var land : landScape = .airplane
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Picker("選擇風景", selection: $land) {
+                    ForEach(landScape.allCases, id: \.self) {
+                        Text($0.rawValue)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding()
+                
+                Spacer()
+                chooseLandScapeView(selectLandScape: land)
+                Spacer()
+                
+            }
+            .navigationTitle("風景照")
+        }
+    }
+}
+
+///emum如果遵從CaseIterable可以將enum變成Array
+enum landScape : String, CaseIterable {
+    case airplane = "Airplane"
+    case bicycle = "Bicycle"
+    case train = "Train"
+}
+
+///設定一個chooseLandScapeView遵從View
+///設定一個landScape變數判斷ContentView選擇的landScape種類 接著顯示指定照片
+struct chooseLandScapeView: View {
+    var selectLandScape: landScape
+    var body: some View {
+        switch selectLandScape {
+        case .airplane :
+            landScapeView(landScape: "Airplane")
+        case .bicycle :
+            landScapeView(landScape: "Bicycle")
+        case .train :
+            landScapeView(landScape: "Train")
+        }
+    }
+}
+
+///顯示照片的View，依照landScape傳進來的值判斷顯示照片內容
+struct landScapeView: View {
+    var landScape: String
+    var body: some View {
+        Image(landScape)
+            .resizable()
+            .frame(width: 400, height: 280)
     }
 }
 
